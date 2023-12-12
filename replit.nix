2dcf6472a92182bae08d815a8574ec7bdfd29e59
kim41223114 <144768201+kim41223114@users.noreply.github.com>
2023-12-12 07:24:20 +0000

    更新 First

--- replit.nix
+++ replit.nix
@@ -1,7 +1,13 @@
 { pkgs }: {
     deps = [
+      pkgs.libev
       pkgs.gnuplot
       pkgs.ncurses.dev
       pkgs.gd
     ];
+  env = {
+    PYTHON_LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath [
+      pkgs.libev
+    ];
+  };
 }
